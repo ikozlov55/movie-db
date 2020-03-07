59eee2ec56768f9aa90f68e0939d87d98507c295
Kozlov Ilya <ilya621@yandex.ru>
2020-03-07 15:39:26 +0300

    Минимально запускаемый проект + зависимости

---
+++ MovieDB/MovieDB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MovieDB
+//
+//  Created by Илья Козлов on 07.03.2020.
+//  Copyright © 2020 Илья Козлов. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ MovieDB/MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Илья Козлов on 07.03.2020.
+//  Copyright © 2020 Илья Козлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
